89103ec1a9a6751cff22b030db57bdb442c1393b
Maciej Gustowski <shialike@gmail.com>
2016-03-14 20:10:45 +0100

    Init

---
+++ TrenerManager.Common/StateManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrenerManager.Common.Enums;
+
+namespace TrenerManager.Common
+{
+    public static class StateManager
+    {
+        public static LoginState LoginState { get; set; }
+    }
+}
